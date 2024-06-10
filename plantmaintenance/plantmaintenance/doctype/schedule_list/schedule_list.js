3afe997bd62ee58def84ed646d26c445c8065539
Samiksha Mohekar <samiksha.m@logicaldna.com>
2024-06-10 11:19:02 +0530

    add generate schedule button in scheduled list

--- plantmaintenance/plantmaintenance/doctype/schedule_list/schedule_list.js
+++ plantmaintenance/plantmaintenance/doctype/schedule_list/schedule_list.js
@@ -10,5 +10,53 @@ frappe.ui.form.on("Schedule List", {
             frappe.set_route('Form', 'Task Allocation');
         });
 
-	},
+         
+         
+
+
+        if (frm.fields_dict.generate_schedule) {
+            $(frm.fields_dict.generate_schedule.input).off('click').on('click', () => {
+                frappe.model.clear_table(frm.doc, "scheduled_details");
+
+                let frequency = frm.doc.frequency;
+                let numberOfVisits = frm.doc.number_of_visits;
+
+                let planDate = frappe.datetime.str_to_obj(frm.doc.plan_date);
+                for (let i = 0; i < numberOfVisits; i++) {
+                    let maintenanceDate = new Date(planDate);
+
+                    if (frequency === 'Daily') {
+                        maintenanceDate.setDate(planDate.getDate() + i);
+                    } else if (frequency === 'Weekly') {
+                        maintenanceDate.setDate(planDate.getDate() + i * 7);
+                    } else if (frequency === 'Monthly') {
+                        maintenanceDate.setMonth(planDate.getMonth() + i);
+                    } else if (frequency === 'Yearly') {
+                        maintenanceDate.setFullYear(planDate.getFullYear() + i);
+                    } else if (frequency === 'Random') {
+                         let randomDays = Math.floor(Math.random() * 365) + 1;
+                        maintenanceDate.setDate(planDate.getDate() + randomDays);
+                    }
+
+
+                    maintenanceDate = frappe.datetime.obj_to_str(maintenanceDate);
+
+                    let child = frappe.model.add_child(frm.doc, "scheduled_details");
+
+                    child.no = i + 1;
+                    child.plant = frm.doc.plant;
+                    child.section = frm.doc.section;
+                    child.maintenance_date = maintenanceDate;
+                 }
+
+                 frm.refresh_field("scheduled_details");
+            });
+        }
+
+
+        
+
+         
+},
+
 });
