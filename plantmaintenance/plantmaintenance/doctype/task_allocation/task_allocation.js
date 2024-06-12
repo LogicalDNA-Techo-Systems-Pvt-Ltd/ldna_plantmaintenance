143caea4e69ac8465f92990311d1ba19573bc5d2
SejalPachaghare11 <sejal.p@logicaldna.com>
2024-06-12 15:35:00 +0530

    made changes in Task Allocation and created buttons to load tasks, upload tasks, and download tasks and add a child table task allocation details

--- plantmaintenance/plantmaintenance/doctype/task_allocation/task_allocation.js
+++ plantmaintenance/plantmaintenance/doctype/task_allocation/task_allocation.js
@@ -1,8 +1,20 @@
-// Copyright (c) 2024, LogicalDNA and contributors
-// For license information, please see license.txt
+// // Copyright (c) 2024, LogicalDNA and contributors
+// // For license information, please see license.txt
 
-// frappe.ui.form.on("Task Allocation", {
-// 	refresh(frm) {
 
-// 	},
-// });
+frappe.ui.form.on('Task Allocation', {
+    refresh: function(frm) {
+        frm.add_custom_button(__('Load Tasks'), function() {
+            load_tasks(frm);
+        });
+
+        frm.add_custom_button(__('Download Tasks'), function() {
+            download_tasks(frm);
+        });
+
+        frm.add_custom_button(__('Upload Tasks'), function() {
+            upload_tasks(frm);
+        });
+    }
+});
+
