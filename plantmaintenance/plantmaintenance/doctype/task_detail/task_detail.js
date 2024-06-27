3a2f0af238f9b3a8e8bb7e50285a1ab8d4b7a142
pragatid20 <pragati.d@logicaldna.com>
2024-06-27 14:19:19 +0530

    |#plant_maintenance|task detail changes|

--- plantmaintenance/plantmaintenance/doctype/task_detail/task_detail.js
+++ plantmaintenance/plantmaintenance/doctype/task_detail/task_detail.js
@@ -1,31 +1,31 @@
 // // Copyright (c) 2024, LogicalDNA and contributors
 // // For license information, please see license.txt
 
-// frappe.ui.form.on('Task Detail', {
-//     readings: function(frm) {
-//         const numReadings = frm.doc.readings;
+frappe.ui.form.on('Task Detail', {
+    readings: function(frm) {
+        const numReadings = frm.doc.readings;
         
-//         if (numReadings > 10) {
-//             frappe.msgprint(__('Number of readings must be less than 10.'));
-//             return;
-//         }
+        if (numReadings > 10) {
+            frappe.msgprint(__('Number of readings must be less than 10.'));
+            return;
+        }
         
-//         for (let i = 1; i <= 10; i++) {
-//             const fieldname = `reading_${i}`;
+        for (let i = 1; i <= 10; i++) {
+            const fieldname = `reading_${i}`;
             
-//             if (i <= numReadings) {
-//                 frm.set_df_property(fieldname, 'hidden', false);
-//             } else {
-//                 frm.set_df_property(fieldname, 'hidden', true);
-//             }
-//         }
-//     },
+            if (i <= numReadings) {
+                frm.set_df_property(fieldname, 'hidden', false);
+            } else {
+                frm.set_df_property(fieldname, 'hidden', true);
+            }
+        }
+    },
     
-//     onload: function(frm) {
-//         frm.trigger('readings');
-//     }
+    onload: function(frm) {
+        frm.trigger('readings');
+    }
     
-// })
+})
 frappe.ui.form.on('Task Detail', {
     refresh: function(frm) {
         console.log('After save triggered');
