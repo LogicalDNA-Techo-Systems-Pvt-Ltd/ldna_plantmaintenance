71394cdc57b533b10a5da68dfd6ae22d246881a3
pragatid20 <pragati.d@logicaldna.com>
2024-06-27 12:21:37 +0530

    |#plant_maintenance|task detail changes|

--- plantmaintenance/plantmaintenance/doctype/task_detail/task_detail.js
+++ plantmaintenance/plantmaintenance/doctype/task_detail/task_detail.js
@@ -1,27 +1,86 @@
 // // Copyright (c) 2024, LogicalDNA and contributors
 // // For license information, please see license.txt
 
-frappe.ui.form.on('Task Detail', {
-    readings: function(frm) {
-        const numReadings = frm.doc.readings;
+// frappe.ui.form.on('Task Detail', {
+//     readings: function(frm) {
+//         const numReadings = frm.doc.readings;
         
-        if (numReadings > 10) {
-            frappe.msgprint(__('Number of readings must be less than 10.'));
-            return;
-        }
+//         if (numReadings > 10) {
+//             frappe.msgprint(__('Number of readings must be less than 10.'));
+//             return;
+//         }
         
-        for (let i = 1; i <= 10; i++) {
-            const fieldname = `reading_${i}`;
+//         for (let i = 1; i <= 10; i++) {
+//             const fieldname = `reading_${i}`;
             
-            if (i <= numReadings) {
-                frm.set_df_property(fieldname, 'hidden', false);
-            } else {
-                frm.set_df_property(fieldname, 'hidden', true);
-            }
-        }
-    },
+//             if (i <= numReadings) {
+//                 frm.set_df_property(fieldname, 'hidden', false);
+//             } else {
+//                 frm.set_df_property(fieldname, 'hidden', true);
+//             }
+//         }
+//     },
     
-    onload: function(frm) {
-        frm.trigger('readings');
+//     onload: function(frm) {
+//         frm.trigger('readings');
+//     }
+    
+// })
+frappe.ui.form.on('Task Detail', {
+    refresh: function(frm) {
+        console.log('After save triggered');
+        
+        if (frm.doc.equipment_code) {
+            frappe.call({
+                method: 'frappe.client.get',
+                args: {
+                    doctype: 'Equipment',
+                    name: frm.doc.equipment_code 
+                },
+                callback: function(r) {
+                    console.log(r.message);
+                    if (r.message && !$.isEmptyObject(r.message)) {
+                      
+                        let damage_list = [];
+                        let cause_list = [];
+
+                        
+                        if (r.message.damage && Array.isArray(r.message.damage)) {
+                            r.message.damage.forEach(d => {
+                                damage_list.push(d.damage);
+                            });
+                        }
+
+                       
+                        r.message.cause.forEach(c => {
+                            cause_list.push(c.cause);
+                        });
+
+                       
+                        frm.set_query("damage", function() {
+                            return {
+                                "filters": {
+                                    "equipment_code": frm.doc.equipment_code,
+                                    "damage": ["in", damage_list]
+                                }
+                            };
+                        });
+
+                        
+                        frm.set_query("cause", function() {
+                            return {
+                                "filters": {
+                                    "equipment_code": frm.doc.equipment_code,
+                                    "cause": ["in", cause_list]
+                                }
+                            };
+                        });
+
+                    } else {
+                        frappe.msgprint(__('No equipment found with this name.'));
+                    }
+                }
+            });
+        }
     }
 });
