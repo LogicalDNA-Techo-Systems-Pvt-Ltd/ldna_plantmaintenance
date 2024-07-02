d3740ce177ea609c34fde047f08d31bdc8d9ce72
Samiksha Mohekar <samiksha.m@logicaldna.com>
2024-07-02 14:43:00 +0530

    Made the mandatory field

--- plantmaintenance/plantmaintenance/doctype/activity/activity.py
+++ plantmaintenance/plantmaintenance/doctype/activity/activity.py
@@ -1,9 +1,28 @@
 # Copyright (c) 2024, LogicalDNA and contributors
 # For license information, please see license.txt
 
-# import frappe
+
+
+import frappe
 from frappe.model.document import Document
 
-
 class Activity(Document):
-	pass
+    def validate(self):
+        self.remove_duplicate_parameters()
+
+    def remove_duplicate_parameters(self):
+        unique_parameters = {}
+        duplicates = []
+
+        for param in self.get('parameter'):
+            key = param.parameter
+            if key in unique_parameters:
+                duplicates.append(param)
+            else:
+                unique_parameters[key] = param
+
+        for duplicate in duplicates:
+            self.remove(duplicate)
+
+    def remove(self, param):
+        self.get('parameter').remove(param)
