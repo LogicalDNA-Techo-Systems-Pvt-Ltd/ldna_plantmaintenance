cfa2c01e456c8cf347fdda772c7c7dce5de21cb6
SejalPachaghare11 <sejal.p@logicaldna.com>
2024-07-03 11:11:40 +0530

    implement the load task button

--- plantmaintenance/plantmaintenance/doctype/task_allocation/task_allocation.py
+++ plantmaintenance/plantmaintenance/doctype/task_allocation/task_allocation.py
@@ -6,6 +6,9 @@ from io import BytesIO
 import calendar
 import os
 import openpyxl
+from dateutil.relativedelta import relativedelta
+from datetime import timedelta
+
 
 class TaskAllocation(Document):
     def on_update(self):
@@ -55,40 +58,6 @@ class TaskAllocation(Document):
         }
         return parameter_info
 
-@frappe.whitelist()
-def load_tasks(work_center, plant_section):
-    if not work_center or not plant_section:
-        frappe.throw(_("Please select Work Center and Plant Section before loading tasks."))
-
-    equipment_list = frappe.get_list('Equipment', filters={'work_center': work_center, 'plant_section': plant_section}, fields=['equipment_code', 'equipment_name', 'activity_group'])
-
-    if not equipment_list:
-        frappe.throw(_("No equipment found for the selected Work Center and Plant Section."))
-
-    tasks_to_add = []
-
-    for equipment in equipment_list:
-        if not equipment.activity_group:
-            continue
-
-        activity_group = frappe.get_doc('Activity Group', equipment.activity_group)
-        for activity in activity_group.activity:
-            activity_doc = frappe.get_doc('Activity', activity.activity_name)
-            for parameter in activity_doc.parameter:
-                task_dates = get_task_dates(parameter.frequency, parameter.range)
-                for task_date in task_dates:
-                    tasks_to_add.append({
-                        'equipment_code': equipment.equipment_code,
-                        'equipment_name': equipment.equipment_name,
-                        'date': task_date,
-                        'frequency': parameter.frequency,
-                        'task_status': 'Open',
-                        'activity_name': activity.activity_name,
-                        'parameter': parameter.parameter,
-                        'day': calendar.day_name[task_date.weekday()]
-                    })
-
-    return tasks_to_add
 
 
 
@@ -160,67 +129,7 @@ def upload_tasks_excel_for_task_allocation(file,task_allocation_name):
 
     return True
 
-def get_task_dates(frequency, range_value=None):
-    start_date = getdate(nowdate())
-    dates = []
 
-    if frequency == 'Daily':
-        dates = [add_days(start_date, i) for i in range(15)]
-    elif frequency == 'Weekly':
-        dates = [add_days(start_date, i * 7) for i in range(12)]
-    elif frequency == 'Monthly':
-        dates = [add_months(start_date, i) for i in range(6)]
-    elif frequency == 'Yearly':
-        dates = [add_years(start_date, i) for i in range(1)]
-    elif frequency == 'Randomly':
-        dates = [add_days(start_date, i * range_value) for i in range(5)]
-
-    return dates
-
-@frappe.whitelist()
-def download_tasks_excel_for_task_allocation(tasks):
-    tasks = frappe.parse_json(tasks)
-
-    wb = Workbook()
-    ws = wb.active
-    ws.title = "Tasks"
-
-    headers = ['Equipment Code', 'Equipment Name', 'Activity', 'Parameter','Frequency', 'Assign TO', 'Date', 'Day']
-    ws.append(headers)
-
-    for task in tasks:
-        task_date = getdate(task.get('date'))
-        row = [
-            task.get('equipment_code'),
-            task.get('equipment_name'),
-            task.get('activity_name'),
-            task.get('parameter'),
-            task.get('parameter_type'),
-            task.get('min_value'),
-            task.get('max_value'),
-            task.get('list_text'),
-            task.get('frequency'),
-            task.get('assign_to'),
-            task_date.strftime('%Y-%m-%d') if task_date else None,
-            task.get('day')
-        ]
-        ws.append(row)
-
-    virtual_workbook = BytesIO()
-    wb.save(virtual_workbook)
-    virtual_workbook.seek(0)
-    file_data = virtual_workbook.read()
-
-    file_name = f'Task_Allocation_{nowdate()}.xlsx'
-    file_doc = frappe.get_doc({
-        'doctype': 'File',
-        'file_name': file_name,
-        'content': file_data,
-        'is_private': 0
-    })
-    file_doc.save(ignore_permissions=True)
-
-    return file_doc.file_url
 
 @frappe.whitelist()
 def check_tasks_generated(docname):
@@ -237,69 +146,134 @@ def generate_tasks(docname):
     else:
         return "Task Allocation document not found."
 
+
 @frappe.whitelist()
-def upload_tasks_excel_for_task_allocation(file,task_allocation_name):
-    task_allocation_doc = frappe.get_doc("Task Allocation",task_allocation_name)    
-   
-    folder_path = ''
-    actual_file_name = ''
+def load_tasks(plant, location, functional_location, plant_section, work_center, end_date=None):
+    filters = {
+        "name1": plant,
+        "location": location,
+        "functional_location": functional_location,
+        "plant_section": plant_section,
+        "work_center": work_center
+    }
 
-    if file.startswith("/private/files/"):
-        actual_file_name = file.replace("/private/files/", '')
-        folder_path = os.path.join(os.path.abspath(frappe.get_site_path()), "private", "files")
-    else:
-        actual_file_name = file.replace("/files/", '')
-        folder_path = os.path.join(os.path.abspath(frappe.get_site_path()), "public", "files")
+    equipment_list = frappe.get_all('Equipment', filters=filters, fields=['equipment_code', 'equipment_name', 'activity_group'])
+    setting_doc = frappe.get_single('Setting')
+    start_date = getdate(setting_doc.start_date)
 
-    source_file = os.path.join(folder_path, actual_file_name)
- 
-
-    wb = openpyxl.load_workbook(source_file)
-    sheet = wb.active
-
-    headers = [sheet.cell(row=1, column=i).value for i in range(1, sheet.max_column + 1)]
-    
-    task_allocation_doc.set("task_allocation_details", [])
-
-    for row in range(2, sheet.max_row + 1):
-       
-        row_data = {}
-        is_blank_row = True
-        for col_num in range(1, sheet.max_column + 1):
-            cell_value = sheet.cell(row=row, column=col_num).value
-            if cell_value is not None and cell_value != '':
-                is_blank_row = False
-                break
-
-        if is_blank_row:
-            continue  
+    tasks = []
+    for equipment in equipment_list:
+        activities = frappe.get_all('Activity CT', filters={'parent': equipment.activity_group}, fields=['activity'])
         
-        for col_num in range(1, sheet.max_column + 1):
-            cell_value = sheet.cell(row=row, column=col_num).value
-            row_data[headers[col_num - 1]] = cell_value
+        for activity in activities:
+            activity_details = frappe.get_doc('Activity', activity.activity)
             
-        print(task_allocation_doc.as_dict())
-        task_allocation_doc.append("task_allocation_details", {
-            'equipment_code': row_data.get('Equipment Code'),
-            'equipment_name': row_data.get('Equipment Name'),
-            'activity': row_data.get('Activity'),
-            'parameter': row_data.get('Parameter'),
-            'frequency': row_data.get('Frequency'),
-            'assign_to': row_data.get('Assign TO'),
-            'date': row_data.get('Date'),
-            'day': row_data.get('Day')
-        })
+            parameters = frappe.get_all('Parameter CT', filters={'parent': activity.activity}, fields=[
+                'parameter', 'frequency', 'day_of_month', 'monday', 'tuesday', 'wednesday', 
+                'thursday', 'friday', 'saturday', 'sunday'
+            ])
+            
+            for parameter in parameters:
+                frequency = parameter.frequency
+                dates = []
 
-        task_allocation_doc.save()
-        
-   
-        # equipment_code = row_data.get('Equipment Code')
-        # equipment_name = row_data.get('Equipment Name')
-        # activity = row_data.get('Activity')
-        # parameter = row_data.get('Parameter')
-        # frequency = row_data.get('Frequency')
-        # assign_to = row_data.get('Assign TO')
-        # date = row_data.get('Date')
-        # day = row_data.get('Day')
+                if frequency == 'Daily':
+                    dates = [add_days(start_date, i) for i in range(15)]
 
-    return True
+                elif frequency == 'Weekly':
+                    selected_days = []
+                    if parameter.monday:
+                        selected_days.append('Monday')
+                    if parameter.tuesday:
+                        selected_days.append('Tuesday')
+                    if parameter.wednesday:
+                        selected_days.append('Wednesday')
+                    if parameter.thursday:
+                        selected_days.append('Thursday')
+                    if parameter.friday:
+                        selected_days.append('Friday')
+                    if parameter.saturday:
+                        selected_days.append('Saturday')
+                    if parameter.sunday:
+                        selected_days.append('Sunday')
+
+                    for day in selected_days:
+                        current_date = start_date
+                        while current_date.weekday() != list(calendar.day_name).index(day):
+                            current_date += timedelta(days=1)
+                        dates.append(current_date)
+                        
+                        while current_date <= start_date + timedelta(days=120):
+                            current_date += timedelta(weeks=1)
+                            dates.append(current_date)
+
+                elif frequency == 'Monthly':
+                    day_of_month = parameter.day_of_month or 1
+                    current_date = start_date.replace(day=day_of_month)
+                    if current_date < start_date:
+                        current_date += relativedelta(months=1)
+                    dates = [current_date + relativedelta(months=i) for i in range(6)]
+
+                elif frequency == 'Yearly':
+                    dates = [add_years(start_date, i) for i in range(1)]
+                
+                for date in dates:
+                    if end_date and getdate(date) != getdate(end_date):
+                        continue
+                    date_obj = getdate(date)
+                    task = {
+                        'equipment_code': equipment.equipment_code,
+                        'equipment_name': equipment.equipment_name,
+                        'activity': activity_details.activity_name,
+                        'parameter': parameter.parameter,
+                        'frequency': frequency,
+                        'date': date,
+                        'day': calendar.day_name[date_obj.weekday()]
+                    }
+                    tasks.append(task)
+                        
+    return tasks
+
+@frappe.whitelist()
+def download_tasks_excel_for_task_allocation(tasks):
+    tasks = frappe.parse_json(tasks)
+    
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Tasks"
+
+    headers = ['Equipment Code', 'Equipment Name', 'Activity', 'Parameter', 'Frequency', 'Assign TO', 'Date','Day']
+    ws.append(headers)
+
+    for task in tasks:
+        task_date = getdate(task.get('date'))
+        row = [
+            task.get('equipment_code'),
+            task.get('equipment_name'),
+            task.get('activity'), 
+            task.get('parameter'),
+            task.get('frequency'),
+            task.get('assign_to'), 
+            task_date.strftime('%Y-%m-%d') if task_date else None,
+            task.get('day')
+
+        ]
+
+        ws.append(row)
+
+    virtual_workbook = BytesIO()
+    wb.save(virtual_workbook)
+    virtual_workbook.seek(0)
+    file_data = virtual_workbook.read()
+
+    file_name = f'Task_Allocation_{nowdate()}.xlsx'
+    file_doc = frappe.get_doc({
+        'doctype': 'File',
+        'file_name': file_name,
+        'content': file_data,
+        'is_private': 0
+    })
+    file_doc.save(ignore_permissions=True)
+    
+    return file_doc.file_url
+
