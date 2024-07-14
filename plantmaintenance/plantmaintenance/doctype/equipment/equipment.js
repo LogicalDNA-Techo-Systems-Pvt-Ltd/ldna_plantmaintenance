2c782f2106629bf999ba1df8ee10dd1d305bb71e
Shreyas <shreyasnaik01@gmail.com>
2024-07-14 16:46:13 +0530

    |#Equipment|after_save_event_on_task_detail|

--- plantmaintenance/plantmaintenance/doctype/equipment/equipment.js
+++ plantmaintenance/plantmaintenance/doctype/equipment/equipment.js
@@ -1,14 +1,6 @@
 // Copyright (c) 2024, LogicalDNA and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Equipment", {
-// 	refresh(frm) {
-
-// 	},
-// });
-
-
-
 frappe.ui.form.on('Equipment', {
     plant: function(frm){
         if (frm.doc.plant){
@@ -110,84 +102,10 @@ frappe.ui.form.on('Equipment', {
 
         }
     },
+    onload: function(frm) {
+
+        frm.get_field('task_detail_ct').grid.cannot_add_rows = true;
+        frm.get_field('material_moment_ct').grid.cannot_add_rows = true;
+}
 });
 
-// function filterChildFields(frm, tableName, fieldTrigger, fieldName, fieldFiltered) {
-//     frm.fields_dict[tableName].grid.get_field(fieldFiltered).get_query = function(doc, cdt, cdn) {
-//         var child = locals[cdt][cdn];
-//         return {    
-//             filters:[
-//                 [fieldName, '=', child[fieldTrigger]]
-//             ]
-//         }
-//     }
-// }
-
-
-
-
-
-
-        
-// frappe.ui.form.on("Equipment",{
-//     refresh: function(frm){
-//         frm.get_field("equipment_code").wrapper.style.width = "50%";
-//         frm.get_field("equipment_group").wrapper.style.width = "50%";
-//         frm.get_field("company_code").wrapper.style.width = "50%";
-//         frm.get_field("activity_group").wrapper.style.width = "50%";
-//         frm.get_field("type_of_technical_object").wrapper.style.width = "50%";
-//         frm.get_field("description_of_technical_object").wrapper.style.width = "50%";
-//         frm.get_field("manufacturer_of_asset").wrapper.style.width = "50%";
-//         frm.get_field("equipment_name").wrapper.style.width = "50%";
-
-        
-//     }   
-
-// });
-
-
-
-
-
-
-
-
-
-// frappe.ui.form.on("Equipment", {
-//     onload: function(frm) {
-//         // Define an empty array to store locations
-//         var locations = [];
-
-//         // Fetch locations from Plant doctype based on the selected plant in Equipment
-//         frappe.call({
-//             method: 'frappe.client.get_list',
-//             args: {
-//                 doctype: 'Plant',
-//                 filters: {
-//                     plant: frm.doc.plant  // Filter by the selected plant in Equipment
-//                 },
-//                 fields: ['location']  // Adjust fields as per your requirement
-//             },
-//             callback: function(r) {
-//                 if (r.message && r.message.length > 0) {
-//                     // Populate locations array with fetched locations
-//                     locations = r.message.map(function(plant) {
-//                         return plant.location;
-//                     });
-
-//                     // Set query for location field in Location child table
-//                     frm.fields_dict['location'].grid.get_field('location').get_query = function(doc, cdt, cdn) {
-//                         return {
-//                             filters: {
-//                                 'location': ['in', locations]
-//                             }
-//                         };
-//                     };
-
-//                     // Refresh the form to apply the query
-//                     frm.refresh_field('location');
-//                 }
-//             }
-//         });
-//     }
-// });
