c27c303058ea603f1d0c32d60f7808b6e31b4b6c
Samiksha Mohekar <samiksha.m@logicaldna.com>
2024-07-16 15:32:23 +0530

    add work center field in user doctype

--- plantmaintenance/plantmaintenance/doctype/task_detail/task_detail_permission.py
+++ plantmaintenance/plantmaintenance/doctype/task_detail/task_detail_permission.py
@@ -1,32 +1,30 @@
 
 
 import frappe
-
+ 
 def task_detail_permission(user):
     if not user:
         user = frappe.session.user
 
     user_doc = frappe.get_doc("User", user)
     user_roles = [role.role for role in user_doc.roles]
-
+    user_work_center = user_doc.work_center 
     if 'Maintenance Manager' in user_roles:
         return """
-        `tabTask Detail`.`approver` = '{user}'
+        (`tabTask Detail`.`approver` = '{user}')  
         """.format(user=user)
     elif 'Maintenance User' in user_roles:
         return """
-        `tabTask Detail`.`assigned_to` = '{user}'
-        """.format(user=user)
+        (`tabTask Detail`.`assigned_to` = '{user}' OR `tabTask Detail`.`assigned_to` IS NULL)
+        """.format(user=user, work_center=user_work_center)
     else:
-        return "1 = 0"   
+        return """
+        `tabTask Detail`.`assigned_to` IS NULL
+        """
     
 
-    
+ 
 
 
 
-
-
-
-         
  
