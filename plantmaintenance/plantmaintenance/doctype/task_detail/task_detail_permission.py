41e0334cb3db19c775e387afa7da0df9b3d91b77
Samiksha Mohekar <samiksha.m@logicaldna.com>
2024-07-16 17:44:03 +0530

    create use work center doctype

--- plantmaintenance/plantmaintenance/doctype/task_detail/task_detail_permission.py
+++ plantmaintenance/plantmaintenance/doctype/task_detail/task_detail_permission.py
@@ -1,30 +1,44 @@
 
 
+  
+
+
+
+
 import frappe
- 
+
 def task_detail_permission(user):
-    if not user:
-        user = frappe.session.user
+    user = frappe.session.user
 
     user_doc = frappe.get_doc("User", user)
     user_roles = [role.role for role in user_doc.roles]
-    user_work_center = user_doc.work_center 
+
+    user_work_center = frappe.get_doc("User Work Center", user)
+
+    user_work_centers = [wc.work_center for wc in user_work_center.work_center] if hasattr(user_work_center, 'work_center') else []
+
     if 'Maintenance Manager' in user_roles:
-        return """
-        (`tabTask Detail`.`approver` = '{user}')  
-        """.format(user=user)
+        if user_work_centers:
+            work_centers_condition = ", ".join(["'{0}'".format(wc) for wc in user_work_centers])
+            return """
+            (`tabTask Detail`.`approver` = '{user}') AND (`tabTask Detail`.`work_center` IN ({work_centers_condition}))
+            """.format(user=user, work_centers_condition=work_centers_condition)
+        else:
+            return """
+            (`tabTask Detail`.`approver` = '{user}')
+            """.format(user=user)
     elif 'Maintenance User' in user_roles:
-        return """
-        (`tabTask Detail`.`assigned_to` = '{user}' OR `tabTask Detail`.`assigned_to` IS NULL)
-        """.format(user=user, work_center=user_work_center)
-    else:
-        return """
-        `tabTask Detail`.`assigned_to` IS NULL
-        """
+        if user_work_centers:
+            work_centers_condition = ", ".join(["'{0}'".format(wc) for wc in user_work_centers])
+            return """
+            (`tabTask Detail`.`assigned_to` = '{user}' OR `tabTask Detail`.`assigned_to` IS NULL) AND (`tabTask Detail`.`work_center` IN ({work_centers_condition}))
+            """.format(user=user, work_centers_condition=work_centers_condition)
+        else:
+            return """
+            (`tabTask Detail`.`assigned_to` = '{user}' OR `tabTask Detail`.`assigned_to` IS NULL)
+            """.format(user=user)
     
 
- 
-
-
+    
 
  
