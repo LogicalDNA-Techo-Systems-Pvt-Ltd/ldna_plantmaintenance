013ec855bea4c41c97c113827af7b2763dde8850
TruptiNi <trupti.n@logicaldna.com>
2024-07-17 17:07:02 +0530

    Remove message from Numeric type

--- plantmaintenance/plantmaintenance/doctype/task_detail/task_detail.js
+++ plantmaintenance/plantmaintenance/doctype/task_detail/task_detail.js
@@ -21,7 +21,6 @@ frappe.ui.form.on('Task Detail', {
     //     });
     // },
 
-
     readings: function(frm) {
         const numReadings = frm.doc.readings;
         
@@ -62,7 +61,7 @@ frappe.ui.form.on('Task Detail', {
     },
 
     validate: function(frm) {
-        if (frm.doc.actual_end_date && frm.doc.actual_start_date && frm.doc.actual_end_date < frm.doc.actual_start_date) {
+        if (frm.doc.actual_end_date < frm.doc.actual_start_date) {
             frappe.msgprint(__('Actual End Date should be greater than or equal to Actual Start Date'));
             frappe.validated = false;
         }
@@ -72,106 +71,34 @@ frappe.ui.form.on('Task Detail', {
         const fields = ['reading_1', 'reading_2', 'reading_3', 'reading_4', 'reading_5', 
                         'reading_6', 'reading_7', 'reading_8', 'reading_9', 'reading_10'];
         
-        let validationMessages = []; // Array to store validation messages
-        let readingsCount = parseInt(frm.doc.readings); // Number of readings
+        let readingsCount = frm.doc.readings; 
+        let hasValidationErrors = false; 
         
-        if (frm.doc.type !== 'Breakdown' && frm.doc.parameter_type !== 'Binary' && frm.doc.parameter_type !== 'List' && (isNaN(readingsCount) || readingsCount < 1 || readingsCount > 10)) {
-            frappe.msgprint("Please specify a valid number of readings between 1 and 10.");
-            frappe.validated = false; // Prevent form submission
-            return;
-        }
-        
-        let hasValidationErrors = false; // Flag to indicate if there are validation errors
-        
-        // Loop through only the specified number of readings
-        for (let i = 0; i < readingsCount; i++) {
-            let field = fields[i];
+        if (frm.doc.parameter_type === 'Numeric') {
+            let minRange = frm.doc.minimum_value;
+            let maxRange = frm.doc.maximum_value;
             
-            if (frm.doc[field] !== undefined && frm.doc[field] !== null) {
-                let value = parseFloat(frm.doc[field]).toFixed(2);
-                let numericValue = parseFloat(value);
-                let parameterType = frm.doc.parameter_type;
-                
-                if (parameterType === 'Numeric') {
-                    // Check if value falls within specified range
-                    let minRange = parseFloat(frm.doc.minimum_value);
-                    let maxRange = parseFloat(frm.doc.maximum_value);
-                    
-                    if (isNaN(minRange) || isNaN(maxRange)) {
-                        validationMessages.push("Please specify valid minimum and maximum values for numeric parameters.");
-                        hasValidationErrors = true;
-                        break;
-                    }
-                    
-                    if (numericValue < minRange || numericValue > maxRange) {
-                        validationMessages.push(
-                            `The value for ${frm.fields_dict[field].df.label} must be between ${minRange} and ${maxRange}.`
-                        );
-                        hasValidationErrors = true;
-                    }
-                } else if (parameterType === 'Binary') {
-                    if (frm.doc.acceptance_criteria === 'YES' && numericValue === 0) {
-                        validationMessages.push(
-                            `The value for ${frm.fields_dict[field].df.label} must be 1 as per the acceptance criteria.`
-                        );
-                        hasValidationErrors = true;
-                    } else if (frm.doc.acceptance_criteria === 'NO' && numericValue === 1) {
-                        validationMessages.push(
-                            `The value for ${frm.fields_dict[field].df.label} must be 0 as per the acceptance criteria.`
-                        );
-                        hasValidationErrors = true;
-                    }
+            for (let i = 0; i < readingsCount; i++) {
+                let field = fields[i];
+                let numericValue = frm.doc[field];
+                if (numericValue < minRange || numericValue > maxRange) {
+                    hasValidationErrors = true;
                 }
             }
         }
-        
         if (hasValidationErrors) {
-            // Set the result to 'Fail'
             frm.doc.result = 'Fail';
-            
-            // Show validation messages
-            frappe.confirm(
-                validationMessages.join("<br>"),
-                function() {
-                    // Continue with form submission
-                    frappe.validated = true;
-                },
-                function() {
-                    // Cancel save action
-                    frappe.validated = false;
-                }
-                
-            );
-        }
-        else{
-            frm.doc.result = 'Pass';
-        }
-    },
-    
-    parameter_dropdown: function(frm) {
-        let parameterDropdownValue = frm.doc.parameter_dropdown;
-        let acceptanceCriteriaForList = frm.doc.acceptance_criteria_for_list;
-
-        if (acceptanceCriteriaForList && parameterDropdownValue !== acceptanceCriteriaForList) {
-            frm.doc.result = 'Fail';
-        }
-        else if (acceptanceCriteriaForList && parameterDropdownValue == acceptanceCriteriaForList) {
+        } else {
             frm.doc.result = 'Pass';
         }
     },
 
     refresh: function(frm) {
-        // Locate the 'Material Issued' child table in the 'Inventory' tab
         let material_issued = frm.fields_dict.material_issued;
 
         if (material_issued && !frm.inventory_button_added) {
-            // Ensure the button is added only once
             frm.inventory_button_added = true;
-
-            // Create the button with adjusted top margin
             let button = $('<button class="btn btn-primary btn-xs" style="margin-top: -40px; margin-left: 89%;">Update Stock</button>')
-            
-            // Append the button after the 'Add Row' button
             material_issued.grid.wrapper.find('.grid-footer').append(button);
         }
     }
