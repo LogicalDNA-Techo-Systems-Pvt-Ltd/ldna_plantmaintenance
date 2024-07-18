9628c380c13930689a5dbb120001730b71920aed
ADMIN <gayatri.y@logicaldna.com>
2024-07-18 10:18:05 +0530

    userwise button created

--- plantmaintenance/plantmaintenance/doctype/activity/activity.js
+++ plantmaintenance/plantmaintenance/doctype/activity/activity.js
@@ -5,13 +5,21 @@
 // 	refresh(frm) {
 
 // 	},
-// });
+// }); 
+
 frappe.ui.form.on('Activity', {
     refresh: function(frm) {
-        if (!frm.is_new()) {
+        if (!frm.is_new()&& frappe.user.has_role('System Manager')) {
             frm.add_custom_button(__('Parameter'), function() {
                 frappe.new_doc('Parameter');
             }, __("Create"));
+            frm.add_custom_button(__('Equipment'), function() {
+                frappe.set_route('List', 'Equipment');
+            }, __("View")); 
         }
     },
-});
+}); 
+
+
+
+
