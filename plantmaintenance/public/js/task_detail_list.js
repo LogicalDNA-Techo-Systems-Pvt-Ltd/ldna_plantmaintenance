4aca4c21bb4b4396c42497c2059b7bbaaed16fc2
pragatid20 <pragati.d@logicaldna.com>
2024-08-08 14:22:56 +0530

    |#plant_maintenance|inventory tab and material movement tab changes|

--- plantmaintenance/public/js/task_detail_list.js
+++ plantmaintenance/public/js/task_detail_list.js
@@ -1,10 +1,10 @@
-frappe.listview_settings['Task Detail'] = {
-    onload: function(listview) {
-        listview.page.add_inner_button(__('Load Task'), function() {
-            frappe.msgprint(__('Custom Load Task Button Clicked'));
-        });
-    }
-};
+// frappe.listview_settings['Task Detail'] = {
+//     onload: function(listview) {
+//         listview.page.add_inner_button(__('Load Task'), function() {
+//             frappe.msgprint(__('Custom Load Task Button Clicked'));
+//         });
+//     }
+// };
 
 
 frappe.views.calendar["Task Detail"] = {
