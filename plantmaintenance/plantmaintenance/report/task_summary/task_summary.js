6dac06634d9461be93719cebfa9db74928ea4847
ADMIN <gayatri.y@logicaldna.com>
2024-08-27 15:43:16 +0530

    filter in report,task summary

--- plantmaintenance/plantmaintenance/report/task_summary/task_summary.js
+++ plantmaintenance/plantmaintenance/report/task_summary/task_summary.js
@@ -1,8 +1,16 @@
 // Copyright (c) 2024, LogicalDNA and contributors
 // For license information, please see license.txt
-
 frappe.query_reports["Task Summary"] = {
-	"filters": [
+    "filters": [
+        {
+            "fieldname": "date_filter",
+            "label": __("Date Filter"),
+            "fieldtype": "Select",
+            "options": ["Daily", "Weekly", "Monthly", "Yearly"],
+            "default": "Monthly",
+            "reqd": 1,
+            
+        }
+    ]
 
-	]
 };
