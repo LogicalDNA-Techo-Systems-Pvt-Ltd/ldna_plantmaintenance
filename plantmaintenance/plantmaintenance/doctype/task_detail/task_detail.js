013d8bff19f0290c24073016ca4fd961f1019ad1
pragatid20 <pragati.d@logicaldna.com>
2024-10-25 15:31:04 +0530

    |plant_maintenance|try to reduce size of assign to dialog box|

--- plantmaintenance/plantmaintenance/doctype/task_detail/task_detail.js
+++ plantmaintenance/plantmaintenance/doctype/task_detail/task_detail.js
@@ -512,7 +512,7 @@ frappe.ui.form.on('Task Detail', {
 
                 let dynamicHeight = userCount * 100;
                 if (userCount > 10) {
-                    dynamicHeight = 500; 
+                    dynamicHeight = 300; 
                 }
 
                 dialog.$wrapper.find('.modal-body').css({
